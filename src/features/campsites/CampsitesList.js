f3eb2f0fc7fd92e6fa9288fc7485e343dcfd5df3
Corey Benedict-Osborn <corey.benedict@gmail.com>
2023-02-16 21:28:06 -0500

    3-React - Week 2 - campsiteSlice

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -1,17 +1,20 @@
 import { CAMPSITES } from '../../app/shared/CAMPSITES';
 import { Col, Row } from 'reactstrap';
 import CampsiteCard from './CampsiteCard';
+import { selectAllCampsites } from './campsitesSlice';
 
 const CampsiteList = () => {
+    const campsites = selectAllCampsites();
+
     return (
         <Row className='ms-auto'>
-            {CAMPSITES.map((campsite) => {
-                        return (
-                            <Col md='5' className='m-4' key={campsite.id}>
-                                <CampsiteCard campsite={campsite} />
-                            </Col>
-                        );
-                    })}
+            {campsites.map((campsite) => {
+                return (
+                    <Col md='5' className='m-4' key={campsite.id}>
+                        <CampsiteCard campsite={campsite} />
+                    </Col>
+                );
+            })}
         </Row>
     )
 }

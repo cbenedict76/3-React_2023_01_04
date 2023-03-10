d73d2705bfd4a24611c9f9bfd5bc310502e552b5
Corey Benedict-Osborn <corey.benedict@gmail.com>
2023-03-10 08:05:12 -0500

    3-React - Week 5 - Async Campsites

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Routes, Route } from 'react-router-dom'
 import ContactPage from './pages/ContactPage.js';
 import HomePage from './pages/HomePage.js';
@@ -6,9 +8,16 @@ import Footer from './components/Footer'
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage.js';
 import CampsiteDetailPage from './pages/CampsiteDetailPage.js';
 import AboutPage from './pages/AboutPage'
+import { fetchCampsites } from './features/campsites/campsitesSlice';
 import './App.css';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCampsites());
+  }, [dispatch]);
+
   return (
     <div className="App">
       <Header />

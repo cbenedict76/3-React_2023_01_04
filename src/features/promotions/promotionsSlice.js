6660f147d3db6cbd343a34b0500149073441996d
Corey Benedict-Osborn <corey.benedict@gmail.com>
2023-03-12 09:14:42 -0400

    3-React - Week 5 - Async DisplayList

--- src/features/promotions/promotionsSlice.js
+++ src/features/promotions/promotionsSlice.js
@@ -37,13 +37,17 @@ const promotionsSlice = createSlice({
             state.isLoading = false;
             state.errMsg = action.error ? action.error.message : 'Fetch failed';
         }
-
     }
 })
 
 export const promotionsReducer = promotionsSlice.reducer;
 
 export const selectFeaturedPromotion = (state) => {
-    return state.promotions.promotionsArray.find(
-        (promotion) => promotion.featured);
+    return {
+        featuredItem: state.promotions.promotionsArray.find(
+            (promotion) => promotion.featured
+        ),
+        isLoading: state.promotions.isLoading,
+        errMsg: state.promotions.errMsg
+    };
 };
